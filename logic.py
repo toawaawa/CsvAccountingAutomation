82e3491616c57b76386d7c6c36c771616cf890bb
toawaawa <totolwto@gmail.com>
2025-12-22 12:13:38 +0800

    fix data_not_processed out of bound access bug in logic.py

--- logic.py
+++ logic.py
@@ -14,9 +14,12 @@ COLUMN_BALANCE = "Balance"
 GL_CLOSING = 10200
 
 def data_not_processed(df, i):
-    if i == len(df) - 1:
-        return True
-    return df.loc[i, COLUMN_REFERENCE] != df.loc[i + 1, COLUMN_REFERENCE] and df.loc[i, COLUMN_REFERENCE] != df.loc[i - 1, COLUMN_REFERENCE]
+    curr = df.loc[i, "Reference"]
+
+    prev_diff = True if i == 0 else curr != df.loc[i - 1, "Reference"]
+    next_diff = True if i == len(df) - 1 else curr != df.loc[i + 1, "Reference"]
+
+    return prev_diff and next_diff
 
 def line_count(description):
     return len(description.splitlines())
