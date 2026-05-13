65143c35dad0dbaeac7ae6c2b969df3cf0926c3c
toawaawa <totolwto@gmail.com>
2026-05-13 11:24:40 +0800

    do no company name case

--- gl_acc_identifier.py
+++ gl_acc_identifier.py
@@ -6,19 +6,22 @@ REQUIRED_PERCENTAGE = 0.7 # 70%
 def get_code(data):
     shop_dept, category, company = data[COLUMN_SHOP_DEPT].strip(), data[COLUMN_CATEGORY].strip(), data[COLUMN_COMPANY].strip()
     code = ""
+    # Match g/l code by company
     if COMPANY_MAP.get(company) is None:
         return ""
     code_map = COMPANY_MAP[company]
     total = sum(code_map.values())
     first_code, first_count = next(iter(code_map.items()))
     if first_count / total >= REQUIRED_PERCENTAGE:
-        code += first_code
-    else:
-        if DEPT_CATEGORY_FREQ_MAP.get(shop_dept + category) is None:
-            return ""
-        code_map = DEPT_CATEGORY_FREQ_MAP[shop_dept + category]
-        total = sum(code_map.values())
-        first_code, first_count = next(iter(code_map.items()))
-        if first_count / total >= REQUIRED_PERCENTAGE and first_code != code:
-            code += first_code
-    return code
+        return first_code
+
+    # Match g/l code by department and category
+    if DEPT_CATEGORY_FREQ_MAP.get(shop_dept + category) is None:
+        return ""
+    code_map = DEPT_CATEGORY_FREQ_MAP[shop_dept + category]
+    total = sum(code_map.values())
+    first_code, first_count = next(iter(code_map.items()))
+    if first_count / total >= REQUIRED_PERCENTAGE:
+        return first_code
+
+    return ""
